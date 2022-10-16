c0f97ef1d3ef5ec7064b37619c3e9cb35858e2fb
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-10-16 12:50:01 +0530

    Increasing Array

---
+++ src/arrays/IncreasingArrayTest.java
@@ -0,0 +1,67 @@
+package arrays;
+
+import org.junit.Test;
+
+public class IncreasingArrayTest {
+    //https://leetcode.com/problems/remove-one-element-to-make-the-array-strictly-increasing/
+
+    @Test
+    public void test1(){
+        int[] nums = {1,2,10,5,7,11};
+        System.out.println(canBeInreasing(nums));
+    }
+
+    @Test
+    public void test3() {
+        int[] nums = {3, 1, 2, 5, 7};
+        System.out.println(canBeInreasing(nums));
+    }
+
+    @Test
+    public void test6(){
+        int[] nums = {105,924,32,968};
+        System.out.println(canBeInreasing(nums));
+    }
+
+
+    @Test
+    public void test8(){
+        int[] nums = {2,3,1,2};
+        System.out.println(canBeInreasing(nums));
+    }
+
+    /*
+     *	PseudoCode
+     *	1) Using two pointers left and right and a count variable
+     *	2) if left < right , move both index
+     *	3) Else increase count
+     *  	4) if count >1 return false ; else return true
+     */
+
+
+    private boolean canBeInreasing(int[] nums){
+        if(nums.length==0) return false;
+        int count=0;
+        for(int left=0,right=1;right<nums.length;left++,right++){
+            if(nums[left]>=nums[right]) {
+                count++;
+                if(left!=0 && nums[left-1] >=nums[right])nums[right]=nums[left];
+            }
+            if(count>1)return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
