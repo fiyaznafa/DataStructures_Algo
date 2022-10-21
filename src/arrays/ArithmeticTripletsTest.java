fbfd273b1fc007c8039614ee4ad3bff55f03ca8e
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-10-21 09:10:39 +0530

    Using set

--- src/arrays/ArithmeticTripletsTest.java
+++ src/arrays/ArithmeticTripletsTest.java
@@ -3,7 +3,9 @@ package arrays;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ArithmeticTripletsTest {
 
@@ -44,7 +46,7 @@ public class ArithmeticTripletsTest {
     public void test4(){
         int[] nums={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,92,93,94,95,96,97,98,99,100};
         int diff=10;
-        System.out.println(arithmeticTriplet(nums,diff));
+        System.out.println(arithmeticTriplets(nums,diff));
     }
 
 
@@ -106,6 +108,14 @@ public class ArithmeticTripletsTest {
         }
         return count;
     }
-
+    private int arithmeticTriplets(int[] nums, int diff) {
+        Set<Integer> set = new HashSet<>();
+        int count =0;
+        for(int eachNum: nums){
+            if(set.contains(eachNum-diff)&& set.contains(eachNum-(2*diff)))count++;
+            set.add(eachNum);
+        }
+        return count;
+    }
 
 }
