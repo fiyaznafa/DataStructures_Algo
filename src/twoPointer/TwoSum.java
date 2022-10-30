d63cda81f9d1bc552c4dabb6f471035af3df5278
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-10-30 17:21:47 +0530

    Two Sum Map Optimized

--- src/twoPointer/TwoSum.java
+++ src/twoPointer/TwoSum.java
@@ -88,7 +88,7 @@ public class TwoSum {
         Map<Integer,Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length ; i++) {
-            if(map.containsKey(target - nums[i])&& i!=map.get(target-nums[i])) {
+            if(map.containsKey(target - nums[i])) {
                 return new int[]{map.get(target-nums[i]),i};
             }else{
                 map.put(nums[i],i);
