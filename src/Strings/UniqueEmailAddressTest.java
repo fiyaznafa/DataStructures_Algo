8ef5f291d2da90779023939ff6304218b11d384f
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-11-02 19:28:21 +0530

    Unique Email

---
+++ src/Strings/UniqueEmailAddressTest.java
@@ -0,0 +1,49 @@
+package Strings;
+
+import org.junit.Test;
+import org.testng.Assert;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddressTest {
+
+    @Test
+    public void test1(){
+        String[] emails ={"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+        Assert.assertEquals(2, noUniqueEmail(emails));
+    }
+
+    @Test
+    public void test2() {
+        String[] emails = {"a@leetcode.com", "b@leetcode.com", "c@leetcode.com"};
+        Assert.assertEquals(3, noUniqueEmail(emails));
+    }
+
+
+    @Test
+    public void test3(){
+        String[] emails={"test.email+alex@leetcode.com","test.email.leet+alex@code.com"};
+        Assert.assertEquals(2, noUniqueEmail(emails));
+    }
+
+    private int noUniqueEmail(String[] emails) {
+        Set<String> set = new HashSet<>();
+        for (String email:emails) {
+            set.add(uniqueEmail(email));
+        }
+        return set.size();
+    }
+
+    private String uniqueEmail(String email) {
+        String[] splitEmail = email.split("@");
+        StringBuilder sb = new StringBuilder();
+
+        //removing the plus character if any.
+        String[] localName = splitEmail[0].split("\\+");
+        sb.append(localName[0].replaceAll("\\.",""));
+        sb.append("@");
+        sb.append(splitEmail[1]);
+        return sb.toString();
+    }
+}
