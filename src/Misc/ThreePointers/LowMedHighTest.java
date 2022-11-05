7ec04a7cca4d62f0f9ad63d184f7d2a6a9805f27
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-11-05 18:54:07 +0530

    Low Med High -Dutch Flag

---
+++ src/Misc/ThreePointers/LowMedHighTest.java
@@ -0,0 +1,57 @@
+package Misc.ThreePointers;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class LowMedHighTest {
+
+    /*
+        You have an unsorted array of integers and a function
+        string getCategory(integer), which deterministically returns 1 of three possible strings: "low", "medium", or "high", depending on the input integer.
+        You need to output an array with all the "low" numbers at the bottom, all the "medium" numbers in the middle, and all the "high" numbers at the top. This is basically a partial sort.
+        Within each category, the order of the numbers does not matter.
+        For example, you might be give the array [5,7,2,9,1,14,12,10,5,3].
+        For input integers 1 - 3, getCategory(integer) returns "low", for 4 - 10 it returns "medium," and for 11 - 15 it returns "high".
+        You could output an array (or modify the given array) that looks like this: [3,1,2,5,5,9,7,10,14,12]
+     */
+
+    @Test
+    public void test1(){
+        int[] nums={5,7,2,9,1,14,12,10,5,3};
+        System.out.println(Arrays.toString(arrangeArray(nums)));
+    }
+
+
+    /*
+       Pseudocode - Three pointers/Dutch flag
+        1. With three pointers - low = 0, medium=0 and high = array length
+        2. If value at middle is in the range of low , swap middle value with low value and increment the pointers
+        3. If value at middle is int the high range, swap middle value with high value and decrement the high value
+     */
+
+    private int[] arrangeArray(int[] nums) {
+        int low=0,middle=0,high= nums.length-1,temp;
+        while(middle<=high) {
+            if (getCategory(nums[middle]).equals("low")) {
+                temp = nums[low];
+                nums[low++] = nums[middle];
+                nums[middle++] = temp;
+            }
+            if (getCategory(nums[middle]).equals("high")) {
+                temp = nums[high];
+                nums[high--] = nums[middle];
+                nums[middle] = temp;
+            } else middle++;
+        }
+        return nums;
+    }
+
+    private String getCategory(int n){
+        if(n<=3)return "low";
+        else if(n<=10) return "medium";
+        else return "high";
+    }
+
+
+}
