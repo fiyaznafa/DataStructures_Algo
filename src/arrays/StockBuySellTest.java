e698939b72efe5041550205fd540c1a9ed1eba05
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-12-15 19:28:06 +0530

    Buy Sell Stock

---
+++ src/arrays/StockBuySellTest.java
@@ -0,0 +1,29 @@
+package arrays;
+
+import org.junit.Test;
+
+public class StockBuySellTest {
+
+    @Test
+    public void test1(){
+        int[] prices={7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+    }
+
+
+    @Test
+    public void test2(){
+        int[] prices={7,6,4,3,1};
+        System.out.println(maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int buy = prices[0], profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > buy)
+                profit = Math.max(profit, prices[i] - buy);
+            else buy = prices[i];
+        }
+        return profit;
+    }
+}
