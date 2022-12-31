925785f9c5ea1574ea3c8c07260c3a1374d4fd09
fiyaznafa <33245610+fiyaznafa@users.noreply.github.com>
2022-12-31 08:36:07 +0530

    Chess Board

---
+++ src/Strings/ChessBoardColorTest.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import org.junit.Test;
+
+public class ChessBoardColorTest {
+
+    @Test
+    public void test1(){
+        String s1 = "a9";
+        System.out.println(squareIsWhite(s1));
+    }
+
+    public boolean squareIsWhite(String coordinates) {
+
+        int firstValue =coordinates.charAt(0)-'a';
+        int secondValue = coordinates.charAt(1)-'0';
+        System.out.println(secondValue);
+
+        if(firstValue%2==0 && secondValue%2==0)return true;
+        else if(firstValue%2==1 && secondValue%2==1)return true;
+        else return false;
+    }
+}
